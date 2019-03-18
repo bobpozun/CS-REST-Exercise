fec72e1908fba3e044e3e75635266c476fab26a5
Robert Pozun <robert.pozun@paypal.com>
2019-03-18 13:26:22 -0400

    Remove exercise from TestClass

--- TestClass.cs
+++ TestClass.cs
@@ -1,4 +1,4 @@
-namespace CS_REST_Exercise
+﻿namespace CS_REST_Exercise
 {
     using RestSharp;
     using System;
@@ -15,11 +15,6 @@ namespace CS_REST_Exercise
         public void test_method()
         {
             // Write code here:
-            // Ping the api, assert a 201/Created status code
-            // Get an auth token and store it, assert 200/OK response with token in body
-            // Create a booking, assert 200/OK response
-            // Delete the booking, assert a 201/Created status code
-
         }
     }
 }
